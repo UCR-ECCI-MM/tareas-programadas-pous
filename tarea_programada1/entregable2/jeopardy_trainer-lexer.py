388da332cbb5bbf6dcd31c419f8fbdb91133dbf7
mafe-andres <maria.andres.monge@gmail.com>
2024-04-03 20:37:16 -0600

    Converted regexes to functions due to priority matching. Added file path as program argument. Cowork.

--- tarea_programada1/entregable2/jeopardy_trainer-lexer.py
+++ tarea_programada1/entregable2/jeopardy_trainer-lexer.py
@@ -3,6 +3,7 @@
 # ------------------------------------------------------------
 from pathlib import Path
 import ply.lex as lex
+import sys
 
 # List of token names.   This is always required
 tokens = (
@@ -11,61 +12,114 @@ tokens = (
     "RBRACKET", # ]
     "LBRACE", # {
     "RBRACE", # }
-    "QUOTES", # "
     "COLON", # :
     "COMMA",
+    "QUOTES",
     # Big labels
     "CATEGORIESLABEL", 
     "QUESTIONSLABEL", 
     # Elements
-    "TEXT",  # ['":!?,\.\;\[\]\{\}\(\)\- \\\/&%#\w]+
     "AIRDATE", # (20[01]\d|19[89]\d)-(0[1-9]|1[0-2])-([0-2]\d|3[01])
-    "VALUE", # ^\$\d+$
-    "SHOWNUMBER", #^\d+$
+    "VALUE", # \\d+
+    "SHOWNUMBER", #\d+
     # "Tiny labels"
     "CATEGORYLABEL",
     "AIRDATELABEL",
     "QUESTIONLABEL",
     "VALUELABEL",
     "ANSWERLABEL",
-    "ROUNDLABEL", #^round$
+    "ROUNDLABEL", #round
     "SHOWNUMLABEL",
+    "TEXT",
 )
 
-# Regular expression rules for simple tokens
-t_LBRACKET = r'^\[$'
-t_RBRACKET = r'^\]$'
-t_LBRACE = r'^\{$'
-t_RBRACE = r'^\}$'
-t_QUOTES = r'^\"$' # Check
-t_COLON = r'^\:$'
-t_COMMA = r'^\,$'
+def t_CATEGORIESLABEL(t):
+    r'"categories"'
+    return t
 
-t_CATEGORIESLABEL = r'^categories$'
-t_QUESTIONSLABEL = r'^questions$'
+def t_QUESTIONSLABEL(t):
+    r'"questions"'
+    return t
 
-t_TEXT = r'[\'\"\:\!\?\,\.\;\[\]\{\}\(\)\- \\\/&%#\w]+' # Check
-t_AIRDATE = r'(20[01]\d|19[89]\d)-(0[1-9]|1[0-2])-([0-2]\d|3[01])'
-t_VALUE = r'^\$\d+$'
-t_SHOWNUMBER = r'^\d+$'
+def t_CATEGORYLABEL(t):
+    r'"category"'
+    return t
 
-t_CATEGORYLABEL = r'^category$'
-t_AIRDATELABEL = r'^air_date$'
-t_QUESTIONLABEL = r'^question$'
-t_VALUELABEL = r'^value$'
-t_ANSWERLABEL = r'^answer$'
-t_ROUNDLABEL = r'^round$'
-t_SHOWNUMLABEL = r'^show_number$'
+def t_AIRDATELABEL(t):
+    r'"air_date"'
+    return t
+
+def t_QUESTIONLABEL(t):
+    r'"question"'
+    return t
+
+def t_VALUELABEL(t):
+    r'"value"'
+    return t
+
+def t_ANSWERLABEL(t):
+    r'"answer"'
+    return t
+
+def t_ROUNDLABEL(t):
+    r'"round"'
+    return t
+
+def t_SHOWNUMLABEL(t):
+    r'"show_number"'
+    return t
+
+def t_QUOTES(t):
+    r'\"'
+    return t
+
+def t_COMMA(t):
+    r'\,'
+    return t
+
+def t_COLON(t):
+    r'\:'
+    return t
+
+def t_LBRACKET(t):
+    r'\['
+    return t
+
+def t_RBRACKET(t):
+    r'\]'
+    return t
+
+def t_LBRACE(t):
+    r'\{'
+    return t
+
+def t_RBRACE(t):
+    r'\}'
+    return t
+
+def t_AIRDATE(t):
+    r'(20[01]\d|19[89]\d)-(0[1-9]|1[0-2])-([0-2]\d|3[01])'
+    return t
+
+def t_VALUE(t):
+    r'\\d+'
+    return t
+
+def t_SHOWNUMBER(t):
+    r'\d+'
+    return t
+
+def t_TEXT(t):
+    r'.+?(?=(?<!\\)")' # Check
+    return t
 
 # Define a rule so we can track line numbers
 def t_newline(t):
     r"\n+"
     t.lexer.lineno += len(t.value)
 
-
 # A string containing ignored characters (spaces and tabs)
 t_ignore = " \t"
-
 
 # Error handling rule
 def t_error(t):
@@ -73,9 +127,9 @@ def t_error(t):
     t.lexer.skip(1)
 
 
-def get_file_content():
+def get_file_content(path):
     # Get the parent directory of the current script and add the data file name
-    file_path = Path(__file__).parent / "data.json"
+    file_path = Path(__file__).parent / path
     # Get content from the file and return it as a string
     try:
         return open(file_path, "r", encoding="utf8").read()
@@ -83,18 +137,24 @@ def get_file_content():
         return f"'{file_path}' not found."
 
 
-# Build the lexer
-lexer = lex.lex()
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <path>")
+        return
 
-# Input extracted from the file
-data = get_file_content()
+    path = sys.argv[1]
+    # Build the lexer
+    lexer = lex.lex()
 
-# Give the lexer some input
-lexer.input(data)
+    data = get_file_content(path)
 
-# Tokenize
-while True:
-    tok = lexer.token()
-    if not tok:
-        break  # No more input
-    print(tok)
+    lexer.input(data)
+
+    while True:
+        tok = lexer.token()
+        if not tok:
+            break  # No more input
+        print(tok)
+
+if __name__ == "__main__":
+    main()
