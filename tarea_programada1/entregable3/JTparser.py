e28917227150f5e250e31243f1117c0216c36bc8
mafe-andres <maria.andres.monge@gmail.com>
2024-04-17 22:50:37 -0600

    Change prints to write file function. Close #17

--- tarea_programada1/entregable3/JTparser.py
+++ tarea_programada1/entregable3/JTparser.py
@@ -7,6 +7,19 @@ import sys
 from JTlexer import tokens
 
 
+def get_file_content(path):
+    file_path = Path(__file__).parent / path
+    try:
+        return open(file_path, "r", encoding="utf8").read()
+    except FileNotFoundError:
+        return f"'{file_path}' not found."
+    
+def write_to_file(path, content):
+    file_path = Path(__file__).parent / path
+    file = open(file_path, "a", encoding="utf8")
+    file.write(content)
+    file.close()
+
 def p_file(t):
     'file : LBRACE CATEGORIESLABEL COLON LBRACKET categories RBRACKET COMMA QUESTIONSLABEL COLON  LBRACKET questions RBRACKET RBRACE'
     t[0] = t[1] + t[2] + t[3] + t[4] + t[5] + t[6] + t[7] + t[8] + t[9] + t[10] + t[11] + t[12] + t[13] 
@@ -34,7 +47,7 @@ def p_question(t):
     if t[0] is None:
         t[0] = ""
     for i in range(1, 25):
-        print(t[i])
+        # print(t[i])
         t[0] += t[i]
 
 def p_category(t):
@@ -53,30 +66,19 @@ def p_answer(t):
 def p_error(p):
     print(f'Syntax error at line {p.lineno}: {p.value}')
 
-
-def get_file_content(path):
-    # Get the parent directory of the current script and add the data file name
-    file_path = Path(__file__).parent / path
-    # Get content from the file and return it as a string
-    try:
-        return open(file_path, "r", encoding="utf8").read()
-    except FileNotFoundError:
-        return f"'{file_path}' not found."
-
-
 def main():
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <path>")
+    if len(sys.argv) != 3:
+        print("Usage: python script.py <input_path> <output_path")
         return
 
-    path = sys.argv[1]
-    # Build the lexer
+    input_path = sys.argv[1]
+    output_path = sys.argv[2]
     parser = yacc.yacc()
 
-    data = get_file_content(path)
+    data = get_file_content(input_path)
 
     result = parser.parse(data)
-    print(result)
+    write_to_file(output_path, result)
 
 if __name__ == "__main__":
     main()
